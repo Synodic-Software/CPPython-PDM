f1746eefcd2d6c5880dec9987d3acc510a53bd3a
Asher Norland <asher.norland@gmail.com>
2022-04-11 08:51:32 -0400

    Strong Reference

--- cppython_pdm/plugin.py
+++ cppython_pdm/plugin.py
@@ -19,8 +19,12 @@ class CPPythonPlugin(Interface):
 
     def __init__(self, core: Core) -> None:
 
+        # NOTE: Verbosity is not filled
+        self.configuration = ProjectConfiguration(verbose=bool(core.ui.verbosity))
+
         self.project = None
-        post_install.connect(self.on_post_install)
+
+        post_install.connect(self.on_post_install, weak=False)
 
     def read_generator_data(self, generator_data_type: Type[GeneratorDataType]) -> GeneratorDataType:
         """
@@ -37,22 +41,22 @@ class CPPythonPlugin(Interface):
         """
         TODO
         """
-        verbose = bool(project.core.ui.verbosity)
 
-        if verbose:
-            project.core.ui.echo("CPPython: Entered 'on_post_install'")
-
+        # Attach configuration for CPPythonPlugin callbacks
         self.project = project
+        self.configuration.verbose = bool(project.core.ui.verbosity)
+
+        if self.configuration.verbose:
+            self.print("CPPython: Entered 'on_post_install'")
 
         pdm_pyproject = project.pyproject
 
         if pdm_pyproject is None:
-            if verbose:
-                project.core.ui.echo("CPPython: Project data was not available")
+            if self.configuration.verbose:
+                self.print("CPPython: Project data was not available")
             return
 
-        configuration = ProjectConfiguration(verbose)
-        cppython_project = CPPythonProject(configuration, self, pdm_pyproject)
+        cppython_project = CPPythonProject(self.configuration, self, pdm_pyproject)
 
         cppython_project.install()
 
